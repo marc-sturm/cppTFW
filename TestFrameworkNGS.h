c8b61a9177c85a358d0d788fa80d9d29de7670f2
Leon Schütz <leon.schuetz@med.uni-tuebingen.de>
2024-04-29 08:40:00 +0200

    TestFrameworkNGS.h: added test function to validate hg19 VCFs

--- TestFrameworkNGS.h
+++ TestFrameworkNGS.h
@@ -23,4 +23,22 @@
     }\
 }
 
+#define VCF_IS_VALID_HG19(vcf_file)\
+{\
+	QString ref_file = Settings::string("reference_genome_hg19", true);\
+	if (ref_file!="")\
+	{\
+		QString output;\
+		QTextStream out_stream(&output);\
+		if (!VcfFile::isValid(vcf_file, ref_file, out_stream))\
+		{\
+			TFW::failed() = true;\
+			TFW::message() = "CHECK_VCF_VALID_HG19(" + QByteArray(#vcf_file) + ") failed\n"\
+			+ "location : " + TFW::name(__FILE__) + ":" + TFW::number(__LINE__) + "\n"\
+			+ "message  : " + output.toUtf8();\
+			return;\
+		}\
+	}\
+}
+
 #endif // TESTFRAMEWORK_H
